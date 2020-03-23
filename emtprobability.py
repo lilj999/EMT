6238d1b220cb4a145ff47df93fa68cf04db36ab3
lilj999 <lilj999@users.noreply.github.com>
2020-03-23 23:38:31 +0800

    Update emtprobability.py
    change the output color.

--- emtprobability.py
+++ emtprobability.py
@@ -73,13 +73,13 @@ if __name__ == "__main__":
     
     fig1, ax1 = plt.subplots()
     #plt.figure()
-    p21=ax1.plot(t01,zs1,color='r', linestyle='--', marker='*')
-    p22=ax1.plot(t02,zs2,color='r', linestyle='dashdot', marker='v')
+    p21=ax1.plot(t01,zs1,color='r', linestyle='--', marker='o')
+    p22=ax1.plot(t02,zs2,color='r',  marker='+')
     #p23=ax1.plot(t11,ls11,color='r', linestyle='dotted', marker='^')
-    p23=ax1.plot(t11,ls11,color='y', marker='^')
-    p31=ax1.plot(t12,ls12,color='y',marker='o')
-    p32=ax1.plot(t21,ls21,color='b',marker='^')
-    p33=ax1.plot(t22,ls22,color='b',marker='o')
+    p23=ax1.plot(t11,ls11,color='y',linestyle='--', marker='o')
+    p31=ax1.plot(t12,ls12,color='y',marker='+')
+    p32=ax1.plot(t21,ls21,color='b',linestyle='--',marker='o')
+    p33=ax1.plot(t22,ls22,color='b',marker='+')
 
     print('SOP without EMT, m=30',t01,zs1)
     print('SOP with EMT, m=30, w=5',t11,ls11)
@@ -104,3 +104,4 @@ if __name__ == "__main__":
     ax1.legend(['Non-EMT m=30','Non-EMT m=60', 'EMT m=30, w=5','EMT m=60, w=5','EMT m=30, w=10', 'EMT m=60, w=10'], loc=0,bbox_to_anchor=(0.55, 0.77))
     ax1.set_xticks(t01,t01)
     plt.show()
+
